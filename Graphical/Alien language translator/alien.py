d0ea1ae0b066dc8a496fdadd62edbf41264121cc
Talha Ahmad <talha.code92@gmail.com>
2025-03-12 09:51:08 +0500

    Update Alien language decoder

--- Graphical/Alien language translator/alien.py
+++ Graphical/Alien language translator/alien.py
@@ -70,10 +70,10 @@ class UnicodeDictionary:
 # Unicode Mapping
 ############################
 # Pre-defined Tuples
-keyboard_characters: tuple = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', ' ')
+keyboard_characters: tuple = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', ' ', '\n', '\t', '\b')
 keyboard_codes: tuple = (97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 58, 59, 60, 61, 62, 63, 64, 91, 92, 93, 94, 95, 96, 123, 124, 125, 126, 32)
-alien_characters: tuple = ('⠮', '⼺', '⨙', '⌖', '⠽', '⊧', '⢗', '⋻', '⸶', '⪂', 'Ⳮ', '⎎', '⌊', '┘', '☶', '⣗', '⚍', '⅛', '⺉', '⧢', '⧤', '⦟', '⪠', '⠟', '⺮', '⣓', 'Ⱨ', 'ℜ', '⡉', '⺕', '∋', '⠰', '⺢', '⟣', '⣋')
-alien_codes: tuple = (10286, 12090, 10777, 8982, 10301, 8871, 10391, 8955, 11830, 10882, 11501, 9102, 8970, 9496, 9782, 10455, 9869, 8539, 11913, 10722, 10724, 10655, 10912, 10271, 11950, 10451, 11367, 8476, 10313, 11925, 8715, 10288, 11938, 10211, 10443)
+alien_characters: tuple = ('⠮', '⼺', '⨙', '⌖', '⠽', '⊧', '⢗', '⋻', '⸶', '⪂', 'Ⳮ', '⎎', '⌊', '┘', '☶', '⣗', '⚍', '⅛', '⺉', '⧢', '⧤', '⦟', '⪠', '⠟', '⺮', '⣓', 'Ⱨ', 'ℜ', '⡉', '⺕', '∋', '⠰', '⺢', '⟣', '⣋', '⡥')
+alien_codes: tuple = (10286, 12090, 10777, 8982, 10301, 8871, 10391, 8955, 11830, 10882, 11501, 9102, 8970, 9496, 9782, 10455, 9869, 8539, 11913, 10722, 10724, 10655, 10912, 10271, 11950, 10451, 11367, 8476, 10313, 11925, 8715, 10288, 11938, 10211, 10443, 10341)
 
 # The empty unicode dictionary list
 unicode_dictionary: list[UnicodeDictionary] = []
@@ -111,6 +111,13 @@ def convert_to_alien(message: str) -> str:
             raise ValueError(str(f'Invalid character found {character} having unicode {ord(character)}'))
     return alien
 
+def is_unique() -> bool:
+    """ Checks if all unicode characters are unique """
+    for alien_character in alien_characters:
+        if alien_characters.count(alien_character) > 1:
+            return False
+    return True
+
 def print_unicode_dictionary():
     """ Print all members in unicode_dictionary """
     for unicode_dict in unicode_dictionary:
@@ -124,6 +131,7 @@ if __name__ == '__main__':
     #         print('>'*4, unicode, value, unicodedata.name(unicode, "Unknown Character"))
     #     print()
     print_unicode_dictionary()
+    print("\nUnique:", is_unique())
 
     # print(convert_to_alien('it is not raining today'))
     # print(convert_to_alien('my name is talha ahmad'))
