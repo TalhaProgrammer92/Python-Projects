96944e502a35de31dfad75b89caf592f9dcc3142
Talha Ahmad <talha.code92@gmail.com>
2025-04-22 09:03:56 +0500

    Update Chess by Database

--- Console/Chess Game/game.py
+++ Console/Chess Game/game.py
@@ -1,6 +1,7 @@
 import PyMisc.color as clr
 import os
 import time
+import sqlite3 as sql
 
 
 ########################
@@ -339,6 +340,85 @@ class Board:
 
 
 ########################
+# Database System
+########################
+class Database:
+    def __init__(self, file: str):
+        """ Constructor """
+        # If folder doesn't exist
+        if not os.path.exists('save'):
+            os.mkdir('save')
+
+        # Create database objects
+        self.database = sql.connect(f'save/{file}')
+        self.cursor = self.database.cursor()
+
+    # Commit Query
+    def commit(self, query: str) -> None:
+        """ Method to commit queries to database """
+        self.cursor.execute(query)
+        self.database.commit()
+
+    # Table Maker
+    def createTable(self, name: str, header: list[str]) -> None:
+        """ Method to create table """
+        self.commit(f"""
+            CREATE TABLE IS NOT EXISTS {name}(
+                --- Header
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                {','.join(head + ' INTEGER NOT NULL' for head in header)}
+            )
+        """)
+
+    # Data Insertion
+    def insertDataTo(self, table: str, header: list[str], values: list[int] | list[str]) -> None:
+        """ Method to insert data into table """
+        self.commit(f"INSERT INTO {table} {tuple(header)} VALUES {tuple(values)}")
+
+    # Data Retrieval
+    def retrieveDataFrom(self, table: str, header: list[str], condition: str = "") -> list:
+        """ Method to retrieve data from table """
+        # Query
+        query: str = f"SELECT {','.join(header) if len(header) > 1 else ''.join(header)} FROM {table}"
+
+        # Condition
+        if len(condition) > 0:
+            query += f" WHERE {condition}"
+
+        # Fetch data
+        self.cursor.execute(query)
+        data: list = self.cursor.fetchone()
+        return data
+
+    # Data Update
+    def updateDataOf(self, table: str, header: list[str], values: list[int] | list[str], condition: str = "") -> None:
+        """ Method to update data of a table """
+        # Query
+        query: str = f"UPDATE {table} SET "
+
+        # Adding data that will be updated
+        if isinstance(header, str) or len(header) == 1:
+            query += f"{header} = '{values}'" if isinstance(values, str) else f"{header} = {values}"
+        else:
+            query += ', '.join(
+                f"{header[i]} = '{v}'" if isinstance(v, str) else f"{header[i]} = {v}"
+                for i, v in enumerate(values)
+            )
+
+        # Condition
+        if len(condition) > 0:
+            query += f" WHERE {condition};"
+
+        # Query commit
+        self.commit(query)
+
+    # Data Deletion
+    def removeDataFrom(self, table: str, condition: str) -> None:
+        """ Method to remove data from a table """
+        self.commit(f"DELETE FROM {table} WHERE {condition}")
+
+
+########################
 # Main Point
 ########################
 if __name__ == '__main__':
@@ -346,24 +426,24 @@ if __name__ == '__main__':
     board.clear()
     board.display()
 
-    head: Heading = Heading(
-        message=Message(Text('Main', clr.property(clr.foreground.bright_green()))),
-        decorator=Text('$', clr.property(
-            clr.foreground.bright_magenta(),
-            None,
-            [clr.style.bold()]
-        ))
-    )
-
-    head.message.text.append(Text('Menu', clr.property(clr.foreground.bright_yellow())))
-
-    # head.display()
-
-    menu: Menu = Menu(head)
-    menu.options.extend([
-        Text('New Game', clr.property(clr.foreground.bright_red(), None, clr.style.italic())),
-        Text('Load Game', clr.property(clr.foreground.bright_red(), None, clr.style.italic())),
-        Text('Exit Game', clr.property(clr.foreground.bright_red(), None, clr.style.italic()))
-    ])
-
-    menu.display()
+    # head: Heading = Heading(
+    #     message=Message(Text('Main', clr.property(clr.foreground.bright_green()))),
+    #     decorator=Text('$', clr.property(
+    #         clr.foreground.bright_magenta(),
+    #         None,
+    #         [clr.style.bold()]
+    #     ))
+    # )
+    #
+    # head.message.text.append(Text('Menu', clr.property(clr.foreground.bright_yellow())))
+    #
+    # # head.display()
+    #
+    # menu: Menu = Menu(head)
+    # menu.options.extend([
+    #     Text('New Game', clr.property(clr.foreground.bright_red(), None, clr.style.italic())),
+    #     Text('Load Game', clr.property(clr.foreground.bright_red(), None, clr.style.italic())),
+    #     Text('Exit Game', clr.property(clr.foreground.bright_red(), None, clr.style.italic()))
+    # ])
+    #
+    # menu.display()
