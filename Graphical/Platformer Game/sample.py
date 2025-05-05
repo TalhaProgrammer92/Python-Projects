b831a953a4ee082067e339de27d45235d693969f
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-05 19:19:22 +0500

    Update Platformer

--- Graphical/Platformer Game/sample.py
+++ Graphical/Platformer Game/sample.py
@@ -18,6 +18,20 @@ class Vector:
     def get(self):
         return (self.y, self.x)
 
+###################
+# Sprite Class
+###################
+class Sprite:
+    def __init__(self, path: str):
+        self.path: str = path
+    
+    def load(self) -> pg.Surface:
+        """ Load the sprite from the given path """
+        return pg.image.load(self.path).convert_alpha()
+    
+    def rotate(self, angle: int) -> pg.Surface:
+        """ Load & Rotate the sprite at certain angle from the given path """
+        return pg.transform.rotate(self.load, angle)
 
 #########################
 # Testing
