5ef50e8fa0c157890f38080fb3f440bda8fbc094
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-08 19:44:42 +0500

    Update CHAOS Cipher

--- Console/CHAOS Cipher/layers.py
+++ Console/CHAOS Cipher/layers.py
@@ -4,11 +4,44 @@ class ceaser:
 	"""
 	? Layer 1 - Ceaser Cipher
 
-	* Modes: 2
 	* Encode: Shift +ve
 	* Decode: Shift -ve
 	"""
 
 	@staticmethod
 	def parse(text: str, shift: int) -> str:
-		return ''.join([chr(ord(c) + shift) if c in ascii_letters else c for c in text])
+		result: str = ''
+
+		# * Iterate through each character of the text
+		print(text)
+		for c in text:
+			print(f'{c} -> ', end='')
+			# * If the character is an ascii letter
+			if c in ascii_letters:
+				# * Index of the character in ascii_letters list
+				index = ascii_letters.index(c)
+				
+				# * Do shift
+				for i in range(abs(shift)):
+					# ! If shift is positive
+					if shift > 0:
+						if index == len(ascii_letters) - 1:
+							index = 0
+							continue
+						index += 1
+					
+					# ! If shift is negative
+					else:
+						if index < 0:
+							index = len(ascii_letters) - 1
+							continue
+						index -= 1
+				
+				# ! Assign new shifted character
+				c = ascii_letters[index]
+			
+			print(c)
+			c += result
+		
+		return result
+
--- Console/CHAOS Cipher/program.py
+++ Console/CHAOS Cipher/program.py
@@ -4,7 +4,7 @@ import layers
 shift: int = 3
 
 if '__main__' == __name__:
-	encoded: str = layers.ceaser.parse('Hello, World!', shift)
+	encoded: str = layers.ceaser.parse('Hello, World! abc XYZ', shift)
 	decoded: str = layers.ceaser.parse(encoded, -shift)
-	print(decoded, encoded, sep='\n')
+	# print(decoded, encoded, sep='\n')
 	# print(chr(65), ord('A'))
