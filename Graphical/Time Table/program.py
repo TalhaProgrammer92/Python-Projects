fc49e00edccb4613b22c6c9dadb92f00938d82da
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-11 11:15:44 +0500

    Update

--- Graphical/Time Table/program.py
+++ Graphical/Time Table/program.py
@@ -3,30 +3,50 @@ import tkinter.ttk as ttk
 import tkinter.messagebox as tmsg
 import sqlite3 as sq
 
+#######################
+# Global Variables
+#######################
+DAYS: tuple = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+
 
 #################
 # Time class
 #################
 class Time:
     def __init__(self, hour: int, minute: int, second: int):
-        self.__hour: int = hour if 24 > hour >= 0 else 0
-        self.__minute: int = minute if 60 > minute >= 0 else 0
-        self.__second: int = second if 60 > second >= 0 else 0
-
-    # * Getters
-    @property
-    def hour(self) -> int:
-        return self.__hour
-
-    @property
-    def minute(self) -> int:
-        return self.__minute
-
-    @property
-    def second(self) -> int:
-        return self.__second
+        self.hour: int = hour if 24 > hour >= 0 else 0
+        self.minute: int = minute if 60 > minute >= 0 else 0
+        self.second: int = second if 60 > second >= 0 else 0
 
     # * Representation
     def __repr__(self) -> str:
-        return f'{self.__hour}:{self.__minute}:{self.__second}'
+        return f'{self.hour}:{self.minute}:{self.second}'
 
+
+##################
+# Event class
+##################
+class Event:
+    def __init__(self, name: str, start_time: Time, end_time: Time):
+        self.name: str = name if len(name) > 0 and not name.isspace() else 'Unknown'
+        self.start_time: Time = start_time
+        self.end_time: Time = end_time
+        self.days: list = []
+
+    # * Add days
+    def addDay(self, day: str) -> None:
+        if day not in self.days and day in DAYS:
+            self.days.append(day)
+
+    # * Representation
+    def __repr__(self) -> str:
+        return f""" Event - {self.name}
+Start: {self.start_time.__repr__()}
+End: {self.end_time.__repr__()}
+Days: {[day for day in self.days]}"""
+
+##############
+# Testing
+##############
+if __name__ == '__main__':
+    pass
