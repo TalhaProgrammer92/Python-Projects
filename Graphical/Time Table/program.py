09b801fa37a49a687a87aa26c5f5c8f563ded92a
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-11 11:08:11 +0500

    Update

--- Graphical/Time Table/program.py
+++ Graphical/Time Table/program.py
@@ -9,11 +9,24 @@ import sqlite3 as sq
 #################
 class Time:
     def __init__(self, hour: int, minute: int, second: int):
-        self.__hour: int = hour
-        self.__minute: int = minute
-        self.__second: int = second
+        self.__hour: int = hour if 24 > hour >= 0 else 0
+        self.__minute: int = minute if 60 > minute >= 0 else 0
+        self.__second: int = second if 60 > second >= 0 else 0
 
     # * Getters
     @property
     def hour(self) -> int:
         return self.__hour
+
+    @property
+    def minute(self) -> int:
+        return self.__minute
+
+    @property
+    def second(self) -> int:
+        return self.__second
+
+    # * Representation
+    def __repr__(self) -> str:
+        return f'{self.__hour}:{self.__minute}:{self.__second}'
+
