f64d3a99e9a01730f333aad6da392b673431d1c9
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-11 10:43:09 +0500

    Update

--- Console/CHAOS Cipher/program.py
+++ Console/CHAOS Cipher/program.py
@@ -4,7 +4,7 @@ import layers
 shift: int = 3
 
 if '__main__' == __name__:
-	encoded: str = layers.ceaser.parse('Hello, World! abc XYZ | ABC xyz', shift)
-	decoded: str = layers.ceaser.parse(encoded, -shift)
+	encoded: str = layers.Ceaser.parse('Hello, World! abc XYZ | ABC xyz', shift)
+	decoded: str = layers.Ceaser.parse(encoded, -shift)
 	print(decoded, encoded, sep='\n')
 	# print(chr(65), ord('A'))
